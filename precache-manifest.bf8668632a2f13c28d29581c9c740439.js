e46b99b1672d7e4a9373aa4136f04ae7db55d8ed
lucyelorm <lucyahadzi@gmail.com>
2020-09-13 21:06:52 +0000

    Updates

---
+++ precache-manifest.bf8668632a2f13c28d29581c9c740439.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b020336c99af972eaa810c4fb4dd4e49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a692a75224b7ebe2076e",
+    "url": "/static/css/main.0faaeeda.chunk.css"
+  },
+  {
+    "revision": "c8550a42f4e035516de8",
+    "url": "/static/js/2.a9857d7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a9857d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a692a75224b7ebe2076e",
+    "url": "/static/js/main.e3f4b45d.chunk.js"
+  },
+  {
+    "revision": "89ac118f8d664424e2aa",
+    "url": "/static/js/runtime-main.40fd9fda.js"
+  }
+]);
